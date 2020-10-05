815e8f24d1e113c1e97ac62b1b0b436fc00083d6
GabrielLopez97 <72084552+GabrielLopez97@users.noreply.github.com>
2020-10-04 20:06:28 -0600

    Archivos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_1_ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
